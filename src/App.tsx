4881d1b3edfe9519034959911cec47abe128db3a
eriksgda <erik.ksgda@gmail.com>
2024-05-15 20:24:42 -0300

    add tasks function

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,32 @@
+import { FormEvent, useState } from 'react'
 import './App.css'
 
-// const button: any = document.querySelector('.button-add')
-// const input: any = document.querySelector('input')
-// const totallist: any = document.querySelector('.total-list')
-
-// let items: Array<any> = []
-
-// function AddTask() {
-//    items.push({
-//      task: input.value,
-//      check: false,
-//    })
-
-//   input.value = ''
-
-//   showTasks()
-// }
-
-// function showTasks(){
-//   let newLi= ''
-
-//   items.forEach((item) =>{
-//    newLi = newLi + 
-//     `
-//     <li class="${item.check}">
-//       ${item.tarefa}
-//     </li>
-//     `
-
-//   })
-
-//   totallist.innerHTML = newLi
-
-//   localStorage.setItem('list', JSON.stringify(items))
-// }
-
-// function reloadTasks(){
-//   const tasks = localStorage.getItem('list')
-
-//   if (tasks) {
-//     items = JSON.parse(tasks)
-//   }
-
-//   showTasks()
-// }
-
-// reloadTasks()
-// button.addEventListener('click', AddTask)
-
-
-
-
-
-
 export default function App() {
+  const [task, setTask] = useState('');
+  const [todoList, setTodoList] = useState<string[]>([]);
+
+  function AddTask(event: FormEvent){
+    event.preventDefault();
+
+    setTodoList((oldTodoList) => [...oldTodoList, task]);
+  }
   
   return (
-      <div className='container'>
+      <form className='container' onSubmit={AddTask}>
         <div className='menu'>
-          <input type="text" placeholder='Digite aqui'/>
-          <button className='button-add'>Adicionar</button>
+          <input type="text" placeholder='Digite aqui'
+          onChange={(event) => setTask(event.target.value)}/>
+          <button className='button-add' type='submit'>Adicionar</button>
 
         </div>
         <div className='list'>
           <ul className='total-list'>
-            <li>texto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto testetexto teste</li>
-            <li>texto teste</li>
-
+            {todoList.map((todo) =>{
+              return <li key={todo}>{todo}</li>
+            })}
           </ul>
         </div>
-      </div>
+      </form>
   )
 }
 
